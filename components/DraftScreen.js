f5a56fb2dde95dba16960117bd0f2116f46493d5
MaxLudd <102232469+MaxLudd@users.noreply.github.com>
2023-07-31 18:51:23 +0000

    improved visuals of draft screen

--- components/DraftScreen.js
+++ components/DraftScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, TextInput, View, Button, Alert, StyleSheet } from 'react-native';
+import { Text, TextInput, View, Button, Alert, StyleSheet, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 class DraftScreen extends Component{
@@ -26,9 +26,11 @@ class DraftScreen extends Component{
 
     render(){
         return(
-            <View>
+            <View style={styles.container}>
                 <TextInput style={styles.input} placeholder="Draft" onChangeText={this.handleDraftInput} value={this.state.draft} />
-                <Button title="Save Draft" onPress={() => this.saveDraft()} />
+                <TouchableOpacity style={styles.button} onPress={() => this.saveDraft()}>
+                    <Text style={styles.buttonText}>Save Draft</Text>
+                </TouchableOpacity>
                 <Text style={styles.success}> {this.state.message} </Text>
             </View>
         );
@@ -37,11 +39,36 @@ class DraftScreen extends Component{
 
 const styles = StyleSheet.create({
     input: {
-      height: 100,
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        //borderColor: 'gray',
+        borderRadius: 8,
+        padding: 10,
+        marginBottom: 16,
     },
+    container: {
+        flex: 1,
+        backgroundColor: '#c9eff0',
+        //justifyContent: 'space-between',
+    },
+    button: {
+        margin: 12,
+        borderWidth: 1,
+        borderColor: 'gray',
+        padding: 10,
+        color: 'black',
+        backgroundColor: '#77DD77',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+        marginBottom: 0
+      },
+
+    buttonText: {
+        color: 'black',
+        fontWeight: 'bold'
+      },
     error: {
       margin: 12,
       color: 'red'
